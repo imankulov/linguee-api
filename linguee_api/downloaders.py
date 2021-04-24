5ac10409b947be72f77db72cbe4311e0929b94ae
Roman Imankulov <roman.imankulov@gmail.com>
2021-04-24 13:38:20 +0100

    Add MemoryCache

--- linguee_api/downloaders.py
+++ linguee_api/downloaders.py
@@ -40,6 +40,24 @@ class HTTPXDownloader(IDownloader):
             return response.text
 
 
+class MemoryCache(IDownloader):
+    """
+    Memory cache.
+
+    Exposes the downloader interface, but requires the upstream to work and
+    keeps records in memory.
+    """
+
+    def __init__(self, upstream: IDownloader):
+        self.upstream = upstream
+        self.cache: dict[str, str] = {}
+
+    async def download(self, url: str) -> str:
+        if url not in self.cache:
+            self.cache[url] = await self.upstream.download(url)
+        return self.cache[url]
+
+
 class ExampleDownloader(IDownloader):
     """
     Fake downloader.
