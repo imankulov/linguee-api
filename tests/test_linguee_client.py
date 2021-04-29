40b494d5f05839e4f1005ac724135455fe6214b0
Roman Imankulov <roman.imankulov@gmail.com>
2021-04-29 07:02:11 +0100

    Run smoke tests for all languages

--- tests/test_linguee_client.py
+++ tests/test_linguee_client.py
@@ -1,6 +1,8 @@
 import pytest
 
+from linguee_api.const import LANGUAGE_CODE, LANGUAGES
 from linguee_api.linguee_client import LingueeClient
+from linguee_api.models import SearchResult
 
 
 @pytest.mark.asyncio
@@ -11,3 +13,15 @@ async def test_linguee_client_should_redirect_on_not_found(
         query="constibado", src="pt", dst="en", guess_direction=False
     )
     assert search_result.query == "constipado"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("lang", list(LANGUAGES.keys()))
+async def test_linguee_client_should_process_test_requests(
+    linguee_client: LingueeClient,
+    lang: LANGUAGE_CODE,
+):
+    search_result = await linguee_client.process_search_result(
+        query="test", src="en", dst=lang, guess_direction=False
+    )
+    assert isinstance(search_result, SearchResult)
