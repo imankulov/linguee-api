85ba4fde1bfcf86fa56443060d5da9dcb28568b8
Roman Imankulov <roman.imankulov@gmail.com>
2021-04-29 07:01:17 +0100

    Remove examples directory and let pytest test run online

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,38 @@
-import pathlib
-
 import pytest
+from pydantic import BaseSettings, Field
 
+from linguee_api.config import settings
+from linguee_api.const import PROJECT_ROOT
 from linguee_api.downloaders.error_downloader import ErrorDownloader
 from linguee_api.downloaders.file_cache import FileCache
+from linguee_api.downloaders.httpx_downloader import HTTPXDownloader
+from linguee_api.downloaders.interfaces import IDownloader
 from linguee_api.linguee_client import LingueeClient
 from linguee_api.parsers import XExtractParser
 
 
-@pytest.fixture
-def examples_dir() -> pathlib.Path:
-    return pathlib.Path(__file__).parents[1] / "examples"
+class PytestSettings(BaseSettings):
+    """Specific settings for pytest."""
+
+    offline: bool = Field(default=False, description="Run tests offline")
+
+    @property
+    def downloader(self) -> IDownloader:
+        return ErrorDownloader() if self.offline else HTTPXDownloader()
+
+    class Config:
+        env_prefix = "pytest_"
+        env_file = (PROJECT_ROOT / ".env").as_posix()
+
+
+pytest_settings = PytestSettings()
 
 
 @pytest.fixture
-def examples_downloader(examples_dir) -> FileCache:
-    return FileCache(cache_directory=examples_dir, upstream=ErrorDownloader())
+def examples_downloader() -> FileCache:
+    return FileCache(
+        cache_directory=settings.cache_directory, upstream=pytest_settings.downloader
+    )
 
 
 @pytest.fixture
